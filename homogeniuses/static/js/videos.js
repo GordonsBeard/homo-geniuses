aeec360eabfa4bab27d6c5a5e8e2703e9f292827
Gordon Reynolds <thisisgordonsemail@gmail.com>
2024-09-05 12:59:01 -0700

    Adding score bar to video page

--- homogeniuses/static/js/videos.js
+++ homogeniuses/static/js/videos.js
@@ -4,6 +4,7 @@ const homo_button = document.querySelector("button.hvote");
 const genius_button = document.querySelector("button.gvote");
 const homo_votes = document.querySelector("span.hcount");
 const genius_votes = document.querySelector("span.gcount");
+const score_bars = document.querySelectorAll(".score-bar .bar")
 
 
 function cast_vote(evt) {
@@ -34,3 +35,51 @@ function cast_vote(evt) {
 homo_button.addEventListener("click", cast_vote)
 genius_button.addEventListener("click", cast_vote)
 
+function update_bar_widths() {
+    if (score_bars.length != 2) {
+        return;
+    }
+
+    hvotes = score_bars[0].dataset.votes;
+    gvotes = score_bars[1].dataset.votes;
+
+    if (hvotes == 0 && gvotes == 0) {
+        return;
+    }
+
+    genius_width = 0;
+    homo_width = 0;
+
+    if (hvotes == 0) {
+        console.log("Zero homo votes");
+        genius_width = 100;
+        homo_width = 0;
+    }
+    else if (gvotes == 0) {
+        console.log("Zero genius votes");
+        homo_width = 100;
+        genius_width = 0;
+    }
+    else if (hvotes == gvotes) {
+        console.log("Equal votes");
+        homo_width = 50;
+        genius_width = 50;
+    }
+    else {
+        total_votes = Number(hvotes) + Number(gvotes);
+        console.log(`total votes: ${total_votes}`);
+        homo_width = (100 * hvotes) / total_votes;
+        console.log(`homo width: ${homo_width}`);
+        genius_width = 100 - homo_width;
+        console.log(`genius width: ${genius_width}`);
+
+    }
+    console.log(homo_width);
+    console.log(genius_width);
+
+    // homo
+    score_bars[0].style.width = `${homo_width}%`;
+    score_bars[1].style.width = `${genius_width}%`;
+}
+
+update_bar_widths();
